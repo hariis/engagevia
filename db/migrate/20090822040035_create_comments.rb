543398e131d277ec275523675393410cc1aa7a2d
satishs <satish.fnu@gmail.com>
2009-08-21 21:16:10 -0700

    freeze rails

---
+++ db/migrate/20090822040035_create_comments.rb
@@ -0,0 +1,16 @@
+class CreateComments < ActiveRecord::Migration
+  def self.up
+    create_table :comments do |t|
+      t.references :user
+      t.references :post
+      t.text :body
+      t.integer :parent_id
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :comments
+  end
+end
