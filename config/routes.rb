a6e6a5278efd0bf2de74399e256bef401ff7891e
satishs <satish.fnu@gmail.com>
2009-08-21 20:06:09 -0700

    added user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :users
+
+  map.root :controller => 'posts', :action => 'index'
   map.resources :posts
 
   # The priority is based upon order of creation: first created -> highest priority.
