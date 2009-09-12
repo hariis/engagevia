58d5287fc8cb8494556ec55b3d1b0e725c7d9519
bagside <bagside@bagvapp.(none)>
2009-09-12 06:03:17 +0200

    remove unused files

--- db/migrate/20090831140522_add_engagements_notify_comment_field.rb
+++
@@ -1,9 +0,0 @@
-class AddEngagementsNotifyCommentField < ActiveRecord::Migration
-  def self.up
-    add_column :engagements, :notify_comment, :boolean, :default => true
-  end
-
-  def self.down
-    remove_column :engagements, :notify_comment
-  end
-end
