287d8482214ee826705a4f8056472206b37bcdd2
bagside <bagside@bagvapp.(none)>
2009-10-02 09:47:58 +0200

    changed local domain

--- config/environment.rb
+++ config/environment.rb
@@ -41,9 +41,6 @@ Rails::Initializer.run do |config|
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
   # config.i18n.default_locale = :de
   DOMAIN = "http://www.engagevia.com/" if ENV['RAILS_ENV'] == 'production'
-   if ENV['RAILS_ENV'] == 'development'
-     host =  "localhost:3000"
-     DOMAIN = "http://" + host + "/"
-   end
+  DOMAIN = "http://localhost:3000/" if ENV['RAILS_ENV'] == 'development'
   
 end
