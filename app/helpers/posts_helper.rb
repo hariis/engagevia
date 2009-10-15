975c8f005e393ce12d3cc93db134ae8d04994f90
satishs <satish.fnu@gmail.com>
2009-10-15 08:05:11 +0200

    added participant count to dashboard

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -2,4 +2,8 @@ module PostsHelper
   def get_comments_count(post)
     pluralize(post.comments.count, "comment")
   end
+
+  def get_participants_count(post)
+    pluralize(post.engagements.count, "participant")
+  end
 end
