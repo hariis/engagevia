05a3008686b35eac2aa731b6d212ff42e0cea33f
bagside <bagside@bagvapp.(none)>
2009-10-25 09:39:41 +0100

    updating participant count

--- app/controllers/engagements_controller.rb
+++ app/controllers/engagements_controller.rb
@@ -123,6 +123,7 @@ class EngagementsController < ApplicationController
 												:endcolor => "#cf6d0f", :duration => 5.0 }
         page.select("#invite-status").each { |b| b.visual_effect :fade, :startcolor => "#fb3f37",
 												:endcolor => "#cf6d0f", :duration => 5.0 }
+        page.replace_html "participant-count", "(#{@post.engagements.size})"
     end
  end
 
@@ -174,6 +175,7 @@ class EngagementsController < ApplicationController
 												:endcolor => "#cf6d0f", :duration => 5.0 }
         page.select("#invite-status").each { |b| b.visual_effect :fade, :startcolor => "#fb3f37",
           :endcolor => "#cf6d0f", :duration => 50.0 }
+        page.replace_html "participant-count", "(#{@post.engagements.size})"
        else
          #page.replace_html "send-status", @error_message
        end
