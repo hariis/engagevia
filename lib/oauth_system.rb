82be8c1db460379277ad044d58843c2e800fe233
hariis <hrajagopal@yahoo.com>
2010-02-26 07:31:55 +0100

    oauth changes

--- lib/oauth_system.rb
+++ lib/oauth_system.rb
@@ -45,14 +45,12 @@ module OauthSystem
 			# Redirect to the show page
       @followers = followers(@user.screen_name)
       RAILS_DEFAULT_LOGGER.error "Followers obtained successfully #{@followers.size}"
-#			respond_to do |format|
-#        format.html {
-#              @engagement = Engagement.new
-#              format.html {  render 'posts/show' }
-#        }
-#       end
-       @engagement = Engagement.new
-       render 'posts/show' and return
+			respond_to do |format|
+        format.html {
+              @engagement = Engagement.new
+              format.html {  render 'posts/show' }
+        }
+       end			  
 		rescue => err
 			# The user might have rejected this application. Or there was some other error during the request.
 			RAILS_DEFAULT_LOGGER.error "Failed to get user info via OAuth--" + err
