39a5881777cad288eb6f05476c5886ebd62d2189
unknown <Satish@.biotronik.com>
2010-03-30 21:45:06 -0700

    test checkin from windows enviornment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::Base
   # Scrub sensitive parameters from your log
    filter_parameter_logging :password
   
-  helper_method :current_user  
+  helper_method :current_user
   
   private  
     def current_user_session  
