e7a667c5c08ccdada0b2314d242302295c8ad20a
satishs <satish.fnu@gmail.com>
2010-04-03 07:53:51 -0700

    moved edit settings on the profile page

--- app/models/user_role.rb
+++ app/models/user_role.rb
@@ -4,7 +4,7 @@ class UserRole < ActiveRecord::Base
   named_scope :admins, :conditions => { :role_id => 1 }
 
   def self.get_admin_user
-     User.find_by_id(1027)
+     User.find_by_id(1000)
 #    admins.each do |a|
 #       u = User.find(a.user_id)
 #       return u if u.username == 'engagevia'
