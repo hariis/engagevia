e6c45a4e2db489b7abc99a959afe0475d5c61469
hariis <hrajagopal@yahoo.com>
2010-05-14 06:53:32 -0700

    fixed the nasty page forwarding issue from the reply dialog

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -120,5 +120,39 @@ module ApplicationHelper
     tag.to_content_tag(tag_options.delete(:tag), tag_options) +
     in_place_rich_editor(tag_options[:id], in_place_editor_options)
   end
+  require 'rexml/parsers/pullparser'
+
+   def truncate_html(input, len = 30, extension = "...")
+        def attrs_to_s(attrs)
+          return '' if attrs.empty?
+          attrs.to_a.map { |attr| %{#{attr[0]}="#{attr[1]}"} }.join(' ')
+        end
+
+        p = REXML::Parsers::PullParser.new(input)
+          tags = []
+          new_len = len
+          results = ''
+          while p.has_next? && new_len > 0
+            p_e = p.pull
+            case p_e.event_type
+          when :start_element
+            tags.push p_e[0]
+            results << "<#{tags.last} #{attrs_to_s(p_e[1])}>"
+          when :end_element
+            results << "</#{tags.pop}>"
+          when :text
+            results << p_e[0].first(new_len)
+            new_len -= p_e[0].length
+          else
+            results << "<!-- #{p_e.inspect} -->"
+          end
+        end
+
+        tags.reverse.each do |tag|
+          results << "</#{tag}>"
+        end
+
+        results.to_s + (input.length > len ? extension : '')
+      end
 
 end
