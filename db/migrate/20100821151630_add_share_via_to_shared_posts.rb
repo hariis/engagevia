b494f44cf7c6acec68f88a0d2f79bf4bd75fb761
hariis <hrajagopal@yahoo.com>
2010-08-24 21:06:01 -0700

    removed migration file

--- db/migrate/20100821151630_add_share_via_to_shared_posts.rb
+++
@@ -1,9 +0,0 @@
-class AddShareViaToSharedPosts < ActiveRecord::Migration
-  def self.up
-    add_column :shared_posts, :shared_via, :string, :default => "facebook", :null => false
-  end
-
-  def self.down
-    remove_column :shared_posts, :shared_via
-  end
-end
