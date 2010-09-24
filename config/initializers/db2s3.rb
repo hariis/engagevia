454a0f78f05de200e16bb4fde44318eb487dcb09
hariis <hrajagopal@yahoo.com>
2010-09-23 20:48:37 -0700

    adding backup db feature

--- config/initializers/db2s3.rb
+++
@@ -1,8 +0,0 @@
- DB2S3::Config.instance_eval do
-    S3 = {
-      :access_key_id     => '08NGNP69YNVTMR6K2M82',
-      :secret_access_key => 'AXzfilUpYL0JlZT2KJYa7qLlmpGEbypqWLTHd+sr',
-      :bucket            => 'engagevia-db'
-    }
-  end
-
