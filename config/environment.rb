993738f7449c0f3b901d520de0fc3d1c0194696f
hariis <hrajagopal@yahoo.com>
2010-09-26 21:37:31 -0700

    removed aws gem

--- config/environment.rb
+++ config/environment.rb
@@ -23,7 +23,6 @@ Rails::Initializer.run do |config|
   config.gem 'twitter'
   config.gem "oauth2", :version => "0.0.8"
   config.gem "db2s3", :source => "http://gemcutter.org"
-  config.gem "aws-s3"
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
   # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
